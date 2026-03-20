b8c04cc66fc46baabe1a87bd5627f76fdfedb044
Nikhil Jadhav <nikhil09jadhav@gmail.com>
2026-03-20 15:02:14 +0530

    Demo API integration done in frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './core/api.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'frontend';
+  api = inject(ApiService);
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.api.getMessage().subscribe((res)=>{
+      console.log(res)
+    });
+  }
+
 }
